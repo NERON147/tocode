98bd09fe37fd3875818ff302ced1167f0b84eed6
NERON147 <faip98.07.5@gmail.com>
2022-08-25 22:40:53 +0300

    qq

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,19 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-          <h1>Hello</h1>
-          <img alt="Vue logo" src="./assets/logo.png" />
+          <!-- <message> </message> можно сократить запись ниже-->
+          <message v-if="message" :message="message" />
+
+          <newNote :note="note" @addNote="addNote" />
+          <div class="note-header">
+            <h1>{{ title }}</h1>
+          
+          <div class="icons">
+          <svg :class="{active: grid}" @click="grid = true" style="cursor:pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
+          <svg :class="{active: !grid}" @click="grid = false" style="cursor:pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
+          </div>
+          </div>
+          <notes :notes="notes" :grid="grid" @remove="removeNote" />
         </div>
       </section>
     </div>
@@ -12,7 +23,68 @@
 </template>
 
 <script>
-export default {};
+import message from "@/components/Message.vue";
+import newNote from "@/components/NewNote.vue";
+import notes from "@/components/Notes.vue";
+
+export default {
+  components: {
+    message,
+    newNote,
+    notes,
+  },
+  data() {
+    return {
+      title: "Notes App",
+      message: null,
+      grid: true,
+      note: {
+        title: "",
+        descr: "",
+      },
+      notes: [
+        {
+          title: "First Note",
+          descr: "Description for first note",
+          date: new Date(Date.now()).toLocaleString(),
+        },
+        {
+          title: "Second Note",
+          descr: "Description for second note",
+          date: new Date(Date.now()).toLocaleString(),
+        },
+        {
+          title: "Third Note",
+          descr: "Description for third note",
+          date: new Date(Date.now()).toLocaleString(),
+        },
+      ],
+    };
+  },
+  methods: {
+    addNote() {
+      // console.log(this.note)
+      let { title, descr } = this.note;
+
+      if (title === "") {
+        this.message = "title can`t be blank!";
+        return false;
+      }
+
+      this.notes.push({
+        title,
+        descr,
+        date: new Date(Date.now()).toLocaleString(),
+      });
+      this.message = null;
+      this.note.title = "";
+      this.note.descr = "";
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Notes.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="notes">
+    <div class="note" v-for="(note, index) in notes" :key="index">
+      <div class="note-header">
+        <p>{{ note.title }}</p>
+        <p style="cursor:pointer" @click="removeNote(index)">x</p>
+      </div>
+      <div class="note-body">
+        <p>{{ note.descr }}</p>
+        <span>{{ note.date }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        notes: {
+            type: Array,
+            required: true,
+        },
+        grid: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    methods: {
+        removeNote (index) {
+            this.$emit('remove', index)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+ .notes {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 40px 0;
+ }
+ .note {
+    width: 48%;
+    padding: 18px 20px;
+    margin-bottom: 20px;
+    background-color: #fff;
+ }
+ .note-header { 
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    h1 {
+        font-size: 32px;
+    }
+    p {
+        font-size: 22px;
+        color: rgb(77, 141, 167);
+    }
+    svg {
+        margin-right: 12px;
+        color: #999;
+        &.active {
+        color: rgb(77, 141, 167);
+        }
+        &:last-child {
+            margin-right: 0;
+        }
+    }
+ }
+ .note-body {
+    p {
+        margin: 20px 0;
+    }
+    span {
+        font-size: 14px;
+        color: #999;
+    }
+ }
+</style>
